4b5064edd92809cff561491495aabdb3e0a818a3
hernanmd <hernan.morales@gmail.com>
2020-03-10 02:19:28 -0300

    Updates

---
+++ precache-manifest.5cc38fd5f00857a25948ebc88a31ae14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acd8b5fc79fd5e42f5510c5c6079c21",
+    "url": "/igevet/index.html"
+  },
+  {
+    "revision": "f6250d1ac98747161e97",
+    "url": "/igevet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fde096e74eabb1398fe6",
+    "url": "/igevet/static/js/2.b1dc5a7b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/igevet/static/js/2.b1dc5a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6250d1ac98747161e97",
+    "url": "/igevet/static/js/main.2e29c639.chunk.js"
+  },
+  {
+    "revision": "1bcdd3fdbb5ea239ef44",
+    "url": "/igevet/static/js/runtime-main.6d050fff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/igevet/static/media/logo.5d5d9eef.svg"
+  }
+]);
